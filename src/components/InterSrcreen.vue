78fac14651c36e09b5b9cb7b952ae06a447d144c
datmt <mtd.sgu@gmail.com>
2024-10-16 15:22:13 +0700

    final

---
+++ src/components/InterSrcreen.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="screen">
+        <div class="screen_inner" :style="{
+            width: `${(((920 - 16 * 4)/Math.sqrt(cardReady.length)-16) * 3 / 4 +16)*Math.sqrt(cardReady.length)}px`
+        }">
+            <card-flip 
+            v-for="(card, index) in cardReady " 
+            :key="index"  
+            :imgBackFace="`/img/${card}.png`"
+            :ref="`card-${index}`"
+            :card="{index , value: card}"
+            @onFlip="checkRules($event)"
+            :cardReady="cardReady"
+        />
+        </div>
+    </div>
+</template>
+
+<script>
+
+import CardFlip from "./CardGame.vue"
+export default {
+    props: {
+        cardReady: {
+            type: Array,
+            default: function() {
+                return [];
+            }
+        },
+    },
+
+    components:{
+        CardFlip,
+    },
+    data(){
+        return {
+            rules: [],
+        };
+    },
+    methods:{
+        checkRules(card){
+            //Nếu đã chọn 2 rồi thì không được chọn gì thêm
+            if(this.rules.length === 1 ){
+                for (let i = 0; i < this.cardReady.length; i++) {
+                    if (this.$refs[`card-${i}`][0]) {
+                        this.$refs[`card-${i}`][0].onDisableTemp();
+                    }
+                }
+                setTimeout(() => {
+                    for (let i = 0; i < this.cardReady.length; i++) {
+                    if (this.$refs[`card-${i}`][0]) {
+                        this.$refs[`card-${i}`][0].offDisableTemp();
+                    }
+                }
+                }, 900);
+            }
+            
+            this.rules.push(card);
+
+            if(this.rules.length === 2 && this.rules[0].value===this.rules[1].value){
+                //right
+                // add class tới component card
+                this.$refs[`card-${this.rules[0].index}`][0].onDisable();
+                this.$refs[`card-${this.rules[1].index}`][0].onDisable();
+                
+                //reset rules
+                this.rules = [];
+
+                //check đã xong game chưa
+                const disableElements = document.querySelectorAll(".screen .card.disable");
+
+                if(disableElements && disableElements.length == this.cardReady.length -2){
+                    // gửi sự kiện cho app biết là game đã xong
+                    setTimeout(() => {
+                        this.$emit("onFinish")
+                    }, 1000);
+                    
+                }
+            }
+            else if(this.rules.length === 2 && this.rules[0].value !==this.rules[1].value){
+                setTimeout(() => {
+                    // đóng 2 thẻ
+                    this.$refs[`card-${this.rules[0].index}`][0].closeFlipCard();
+                    this.$refs[`card-${this.rules[1].index}`][0].closeFlipCard();
+                    
+                    //reset [] rules
+                    this.rules = [];
+
+                }, 900);
+            }
+            else return false;
+
+        },
+        // Hàm disable toàn bộ các thẻ
+        
+    },
+    
+}
+</script>
+
+
+
+<style lang="css" scoped>
+    .screen{
+        width: 100%;
+        height: 100vh;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 2;
+        background-color: var(--dark);
+        color: var(--light);
+
+    }
+    .screen_inner{
+        display: flex;
+        flex-wrap: wrap;
+        margin: 2rem auto;
+    }
+</style>
